2f1df6f5510536683f8f25f9fa4acb8efb84170b
Vlad <begavlad068@gmail.com>
2024-08-19 19:15:15 +0300

    add relations and migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from newspaper_agency import settings
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        ordering = ('username',)
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateField()
+    topics = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    publishers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
