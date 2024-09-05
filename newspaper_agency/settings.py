dd90ed543ecdd6d1eee15c1df592623fc2de2c51
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2024-09-05 10:10:03 +0300

    Revert "Develop"

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,10 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog',
-    'crispy_forms',
-    'crispy_bootstrap4'
-
 ]
 
 MIDDLEWARE = [
@@ -58,7 +54,8 @@ ROOT_URLCONF = 'newspaper_agency.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,7 +63,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'catalog.context_processors.cfg_assets_root',
             ],
         },
     },
@@ -122,20 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-                    ]
-
-ASSETS_ROOT = '/static/assets'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = "catalog.Redactor"
-
-LOGIN_URL = '/registration/login/'
-
-LOGIN_REDIRECT_URL = "/catalog/"
-
-CRISPY_TEMPLATE_PACK = "bootstrap4"
